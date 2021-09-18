c549f0508d88eb31c14348c0200b1ba496f755df
Perl <mlpfevardo@gmail.com>
2021-09-19 01:36:13 +0800

    semicolons kekw

--- src/functions/play-spotfiy.js
+++ src/functions/play-spotfiy.js
@@ -6,35 +6,35 @@ const spotifySearch = async (searchString) => {
   const spotifyLink = 'https://open.spotify.com/';
   if (searchString.startsWith(spotifyLink)) {
     if (searchString.startsWith(`${spotifyLink}track/`)) {
-      console.log(`${spotifyLink}track/`)
+      console.log(`${spotifyLink}track/`);
       const trackId = searchString.replace(`${spotifyLink}track/`, "").split('?')[0];
       console.log("temp track id", trackId);
       try{
-        const data = await spotifyApi.getTrack(trackId)
-        console.log(data)
-        console.log(data.body.artists)
-        console.log(data.body.name)
-        let searchStringForYt = `${data.body.name} ${data.body.artists[0].name}`
-        console.log("searchString", searchStringForYt)
+        const data = await spotifyApi.getTrack(trackId);
+        console.log(data);
+        console.log(data.body.artists);
+        console.log(data.body.name);
+        let searchStringForYt = `${data.body.name} ${data.body.artists[0].name}`;
+        console.log("searchString", searchStringForYt);
         return searchStringForYt;
       }
       catch(e){
-        console.log("spotify get track error", e)
-        throw e
+        console.log("spotify get track error", e);
+        throw e;
       }
     }
     else if (searchString.startsWith(`${spotifyLink}album/`)) {
-      console.log(`${spotifyLink}album/`)
+      console.log(`${spotifyLink}album/`);
       const temp = searchString.replace(`${spotifyLink}album/`, "").split('?')[0];
       console.log("temp album", temp);
-      return ""
+      return "";
       // spotifyApi.getTrack();
     }
     else if (searchString.startsWith(`${spotifyLink}playlist/`)) {
-      console.log(`${spotifyLink}playlist/`)
+      console.log(`${spotifyLink}playlist/`);
       const temp = searchString.replace(`${spotifyLink}album/`, "").split('?')[0];
       console.log("temp playlist", temp);
-      return ""
+      return "";
       // spotifyApi.getTrack();
     }
   }
