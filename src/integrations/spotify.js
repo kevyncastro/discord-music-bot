21b693b09a2983ae561e80be91aa6a6f4a428f2c
Perl <mlpfevardo@gmail.com>
2021-10-17 12:56:37 +0800

    changes

--- src/integrations/spotify.js
+++ src/integrations/spotify.js
@@ -3,8 +3,8 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 
 const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.SPOTIFY_CLIENT_ID ? process.env.SPOTIFY_CLIENT_ID : '',
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET ? process.env.SPOTIFY_CLIENT_SECRET : '',
+  clientId: process.env.SPOTIFY_CLIENT_ID || '',
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET || '',
 });
 
 const getSpotifyAccessToken = async () => {
